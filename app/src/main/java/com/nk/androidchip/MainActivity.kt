43d19807317259af7846799805c36f393b615d94
n-k-d <n.k.d.assist@gmail.com>
2021-09-13 11:14:48 +0530

    dynamic chip

--- app/src/main/java/com/nk/androidchip/MainActivity.kt
+++ app/src/main/java/com/nk/androidchip/MainActivity.kt
@@ -2,10 +2,45 @@ package com.nk.androidchip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editText: EditText
+    private lateinit var addButton: Button
+    private lateinit var chipGroup: ChipGroup
+
+
+    private fun addChip(text: String){
+        val chip = Chip(this)
+        chip.text = text
+
+        chip.isCloseIconVisible = true
+
+        chip.setChipIconResource(R.drawable.ic_baseline_android_24)
+
+        chip.setOnCloseIconClickListener{
+            chipGroup.removeView(chip)
+        }
+
+        chipGroup.addView(chip)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        addButton = findViewById(R.id.button)
+        chipGroup = findViewById(R.id.chipGroup)
+
+        addButton.setOnClickListener{
+            if(!editText.text.toString().isEmpty()){
+                addChip(editText.text.toString())
+
+                editText.setText("")
+            }
+        }
     }
 }
